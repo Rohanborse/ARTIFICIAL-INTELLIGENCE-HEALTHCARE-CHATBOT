8293201a8989336378578f507b00a63e2ca191ee
Rohan Borse <111951030+Rohanborse@users.noreply.github.com>
2023-08-29 16:20:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -466,6 +466,6 @@ def chat_msg():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=False, port=5000)
-    
+    app.run(debug=False, port=8501)
+
     
